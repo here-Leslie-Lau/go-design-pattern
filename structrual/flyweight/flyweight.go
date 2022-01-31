2d8fb2aaaf3923f9042ac843f54ad7e9a106dc08
Leslie-Lau <804738464@qq.com>
2022-02-01 02:28:06 +0800

    flyweight

--- structrual/flyweight/flyweight.go
+++ structrual/flyweight/flyweight.go
@@ -1,1 +1,31 @@
 package flyweight
+
+// dress 享元接口
+type dress interface {
+	getColor() string
+}
+
+// redTeamDress 红队服装享元接口实现
+type redTeamDress struct{}
+
+func (d *redTeamDress) getColor() string {
+	return "red"
+}
+
+// blueTeamDress 蓝队服装享元接口实现
+type blueTeamDress struct{}
+
+func (d *blueTeamDress) getColor() string {
+	return "blue"
+}
+
+// newDressFactory 初始化享元工厂对象
+func newDressFactory() map[string]dress {
+	m := map[string]dress{"red": &redTeamDress{}, "blue": &blueTeamDress{}}
+	return m
+}
+
+// person 队员
+type person struct {
+	dress dress
+}
--- structrual/flyweight/flyweight_test.go
+++ structrual/flyweight/flyweight_test.go
@@ -1,1 +1,22 @@
 package flyweight
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFlyWeight(t *testing.T) {
+	// 获取享元对象工厂
+	f := newDressFactory()
+	// 获取红队成员服装
+	red := &person{dress: f["red"]}
+	fmt.Println(red.dress.getColor())
+	// 获取蓝队成员服装
+	blue := person{dress: f["blue"]}
+	fmt.Println(blue.dress.getColor())
+	// 生成第二名蓝队成员，并比较服装对象是否仍然是同一个
+	blue2 := &person{dress: f["blue"]}
+	if blue2.dress != blue.dress {
+		t.Fail()
+	}
+}
