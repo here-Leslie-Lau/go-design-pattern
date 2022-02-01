5ca2a952234549e6b3135ac3d6afac7e72c67f25
Leslie-Lau <804738464@qq.com>
2022-02-01 16:01:20 +0800

    finished proxy pattern

--- structrual/proxy/proxy_test.go
+++ structrual/proxy/proxy_test.go
@@ -1,1 +1,11 @@
 package proxy
+
+import "testing"
+
+func TestProxy(t *testing.T) {
+	p := newProxyVideo()
+	p.playVideo("春晚")
+	// output:
+	//2022/02/01 16:00:40 logging in proxy_video: 春晚
+	//iQiYiL 春晚
+}
